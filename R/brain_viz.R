789ee4e4a6573905c1bb58f4376971d36b0363e7
Tyson S. Barrett <t.barrett88@gmail.com>
2019-03-28 16:22:50 -0600

    getting some structure

---
+++ R/brain_viz.R
@@ -0,0 +1,14 @@
+#' Brain Visual
+#'
+#' Provides a `ggplot2` figure of the connections of regions of the brain.
+#'
+#' @param obj
+#' @param ...
+#'
+#' @export
+ggplot.connectivity <- function(){
+
+}
+
+
+## Same arguments as ggplot but have it give the brain image for the background with specified brain regions
