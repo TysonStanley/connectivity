e6f2e34e247c2e97ac2c4503b92140dfafce2372
TysonStanley <t.barrett@aggiemail.usu.edu>
2019-09-16 20:08:58 -0600

    updated ignore_folders in import_nirs()

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -7,7 +7,7 @@
 #' @param ... the channels (a named list of numbers)
 #' @param num_channels the number of channels for each of the probes
 #' @param sides should the channel means be split into sides (based on the probe)
-#' @param ignore_folders folders to ignore in the directory in regular expression form, e.g. "first|random" would remove any folders that have "first" or "random" in their names
+#' @param ignore_folders folders to ignore in the directory
 #'
 #' @import fs
 #' @import purrr
@@ -23,7 +23,7 @@ import_nirs <- function(path = "", ..., num_channels = 24, sides = FALSE, ignore
   ## Remove any that aren't a folder
   files <- files[!grepl("\\.xlsx|\\.csv|\\.txt", files)]
   if (!is.null(ignore_folders))
-    files <- files[!grepl(ignore_folders, files)]
+    files <- files[! files %in% ignore_folders]
   ## Load all brodExtract files for each participant
   data_brod <- extract_brod(files, path, num_channels)
 
