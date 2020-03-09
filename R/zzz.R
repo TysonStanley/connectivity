f5e0474026e007490032107754ad999d7190c4a0
Tyson S. Barrett <t.barrett88@gmail.com>
2020-03-09 16:35:40 -0600

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   if(getRversion() >= "2.15.1") {
     utils::globalVariables(c(".", "est", "rowname", "group", "number", "comb", "formula", "x", "y", "x.x", "y.x", "x.y", "y.y",
                              "x_padding", "x_perp", "xend", "xend_perp", "y_padding", "y_perp", "yend", "yend_perp",
-                             "slope", "sig", "pythag", "pvalue", "inv_slope", "outcome", "probe", "coloring"))
+                             "slope", "sig", "pythag", "pvalue", "inv_slope", "outcome", "probe", "coloring", "probe_data"))
   }
   invisible()
 }
